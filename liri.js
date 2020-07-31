2898d7397c3c286895091b45d4080852c9f50c9a
Danny Montano <dsmontano12@gmail.com>
2020-07-31 07:10:00 -0400

    added if and else statements

--- liri.js
+++ liri.js
@@ -1,13 +1,49 @@
 require("dotenv").config();
-var keys = require("./keys");
-var fs = require("fs");
-var inquirer = require("inquirer");
-var Spotify = require("node-spotify-api");
+
 var axios = require("axios");
 
+var keys = require("./keys.js");
 
+var moment = require('moment');
 
+var Spotify = require('node-spotify-api');
 
+var fs = require("fs");
 
+var spotify = new Spotify(keys.spotify);
 
-var spotify = new spotify(keys.spotify);
+// User input
+var userInput = process.argv[2];
+
+var userCommand = process.argv.splice(3).join();
+
+// Statements for user input
+if (userInput === "movie-this") {
+    movieThis(userCommand);
+}
+
+else if (input === "concert-this") {
+    concertThis(userCommand);
+}
+
+else if (input === "spotify-this-song") {
+    spotifyTrack(userCommand);
+}
+else if (input === "do-what-it-says") {
+    doWhatItSays(userCommand);
+}
+else {
+    console.log("Please choose exisiting command");
+}
+// App Logic
+function moveThis(movie) {
+    var movieQuery = movie || "Mr. Nobody"
+
+    var queryURL = "http://www.omdbapi.com/?i=t" + movieQuery + "&y=&plot=short&apikey=397827f2";
+
+    axios.get(queryURL).then(
+        function(response) {
+
+        }
+    )
+}
