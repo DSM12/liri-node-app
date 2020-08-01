b7ce102c6f10653a66b007db924457e4ae41472e
Danny Montano <dsmontano12@gmail.com>
2020-08-01 10:59:15 -0400

    movie and concert functionality works

--- liri.js
+++ liri.js
@@ -4,9 +4,9 @@ var axios = require("axios");
 
 var keys = require("./keys.js");
 
-var moment = require('moment');
+var moment = require("moment");
 
-var Spotify = require('node-spotify-api');
+var Spotify = require("node-spotify-api");
 
 var fs = require("fs");
 
@@ -15,35 +15,79 @@ var spotify = new Spotify(keys.spotify);
 // User input
 var userInput = process.argv[2];
 
-var userCommand = process.argv.splice(3).join();
+var userCommand = process.argv.splice(3).join(" ");
 
 // Statements for user input
 if (userInput === "movie-this") {
     movieThis(userCommand);
 }
 
-else if (input === "concert-this") {
+else if (userInput === "concert-this") {
     concertThis(userCommand);
 }
 
-else if (input === "spotify-this-song") {
+else if (userInput === "spotify-this-song") {
     spotifyTrack(userCommand);
 }
-else if (input === "do-what-it-says") {
-    doWhatItSays(userCommand);
-}
-else {
-    console.log("Please choose exisiting command");
-}
+// else if (userinput === "do-what-it-says") {
+//     doWhatItSays(userCommand);
+// }
+// else {
+//     console.log("Please choose exisiting command");
+// }
+
 // App Logic
-function moveThis(movie) {
+function movieThis(movie) {
     var movieQuery = movie || "Mr. Nobody"
 
-    var queryURL = "http://www.omdbapi.com/?i=t" + movieQuery + "&y=&plot=short&apikey=397827f2";
+    var queryURL = "http://www.omdbapi.com/?t=" + movieQuery + "&y=&plot=short&apikey=trilogy";
+    console.log("========================================================================");
+    axios.get(queryURL).then(
+        function (response) {
+            // console.log(response.data);
+            console.log("Title: " + response.data.Title);
+            console.log("Release Year: " + response.data.Year);
+            console.log("IMDB Rating: " + response.data.imdbRating);
+            console.log("Rotten Tomatoes Rating: " + response.data.Ratings[1].Value);
+            console.log("Country: " + response.data.Country);
+            console.log("Language: " + response.data.Language);
+            console.log("Plot: " + response.data.Plot);
+            console.log("Cast: " + response.data.Actors);
+            console.log("========================================================================");
+        }
+
+    )
+
+}
+
+function concertThis(concert) {
+    var concertQuery = concert || "'The Sign' by Ace of Base"
+
+    var queryURL = "https://rest.bandsintown.com/artists/" + concertQuery + "/events?app_id=codingbootcamp";
 
     axios.get(queryURL).then(
-        function(response) {
+        function (response) {
+            console.log(response.data);
+            // console.log(response.data);
 
         }
     )
 }
+
+function spotifyTrack(track) {
+    var songName = track || "What's My Age Again"
+    spotify
+        .search({ type: "track", query: songName })
+        .then(function (response) {
+            console.log(response.data);
+            // console.log("Artist: " + response.data.tracks.items[0].album.artists[0].name);
+            // console.log("Song: " + response.data.tracks.items[0].name);
+            // console.log("Album: " + response.data.tracks.items[0].album.name);
+            // console.log("Preview: " + response.data.tracks.items[0].preview_url);
+        })
+        .catch(function (err) {
+            console.log(err);
+        });
+
+}
+
